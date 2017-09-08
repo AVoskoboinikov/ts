5ca11d1826a38173b357c92a6ab48672c4ea2936
iwantlearn <iwantlearn@gmail.com>
2017-09-08 21:27:24 +0300

    updates

--- www/alpari/advisers.py
+++ www/alpari/advisers.py
@@ -7,29 +7,39 @@ def alwaysTrue(historyData):
 def alwaysFalse(historyData):
 	return False
 
-def advicerThreeSteps(historyData):
+def adviserThreeSteps(historyData):
 	ok = False
 	
+	# find bottom points where graph changes direction
 	up, down = findUnicorns(historyData[-10:])
 	
+	# check if previous points are like steps
 	if down == sorted(down) and len(down) >= 3:
 		ok = True
 		
-		# maybe this block can be deleted
+		# checks that previous points are not lying in horizontal line
 		for i in range(1, len(down)):
 			if abs(down[i] - down[i-1]) == 0:
 				ok = False
 		
 		if ok:
-			up, down = findUnicorns(historyData[-50: -10])
+			up, down = findUnicorns(historyData)
 			
+			# check if previos graph moving down
 			if down == sorted(down, reverse=True):
 				ok = False
 
 		if ok:
-			up, down = getUnicornIndex(historyData[-50: -10])
+			up, down = getUnicornIndex(historyData)
 
-			if down > up or abs(up - down) < 0.0002:
+			# check that previous graph moves more up, then down
+			if down > up or abs(up - down) <= 0.0001 or abs(up - down) >= 0.0005:
+				ok = False
+
+			if up >= 0.002:
+				ok = False
+
+			if down >= 0.0015:
 				ok = False
 
 		if ok:
@@ -37,6 +47,7 @@ def advicerThreeSteps(historyData):
 			sortedUp = sorted(up)
 			sortedDown = sorted(down)
 
+			# checks that distance between each step is not too big
 			if len(sortedDown) > 0 and len(sortedUp) > 0:
 				x1 = sortedDown[0]
 				x2 = sortedUp[-1]
--- www/alpari/simulate_alpari.py
+++ www/alpari/simulate_alpari.py
@@ -1,28 +1,53 @@
 import pandas as pd
-from advicers import advicerThreeSteps
-from advicers import alwaysTrue
-from advicers import alwaysFalse
+import matplotlib.pyplot as plt
+from advisers import adviserThreeSteps
+from advisers import alwaysTrue
+from advisers import alwaysFalse
 
-audusdFile = 'AUDUSD1M - last year.csv'
+# audusdFile = 'AUDUSD1M - last year.csv'
+# audusdFile = 'AUDUSD1M.csv'
+# audusdFile = 'NZDUSD1M - last year.csv'
+audusdFile = 'NZDUSD1M.csv'
+
+def drawPlot(index, orderTick, ticks, isPositive):
+	y_axis = [float(tick) for tick in ticks]
+	x_axis = [i for i in range(1, len(y_axis) + 1)]
+
+	plt.plot(x_axis, y_axis, 'g-')
+	plt.axvline(len(ticks) - orderTick)
+
+	plotFileName = str(index) + '.png'
+
+	if isPositive == True:
+		plt.savefig('../plots/positive/' + plotFileName)
+
+	if isPositive == False:
+		plt.savefig('../plots/negative/' + plotFileName)
+
+	plt.clf()
+
+
 audusd = pd.read_csv(audusdFile, usecols=['Date', 'Time', 'Open'])
 openAudUsd = audusd['Open'].values
 
 isOrderPlaced = False
 orderPlaceValue = 0
+orderPlaceTick = 0
 totalProfit = 0
 totalLoss = 0
 window = 50
 stopLoss = 0.0007
-stopProfit = 0.001
+stopProfit = 0.0009
+totalLossBiggerThanTotalProfit = 0
 
 for tick in range(window, len(openAudUsd)):
-
 	if isOrderPlaced == False:
 		history = openAudUsd[(tick-window) : tick]
 
-		if advicerThreeSteps(history) == True:
+		if adviserThreeSteps(history) == True:
 			isOrderPlaced = True
 			orderPlaceValue = openAudUsd[tick]
+			orderPlaceTick = tick
 
 	if isOrderPlaced == True:
 		tickDiff = openAudUsd[tick] - orderPlaceValue
@@ -31,10 +56,17 @@ for tick in range(window, len(openAudUsd)):
 			isOrderPlaced = False
 			totalLoss += abs(tickDiff)
 
+			if totalLoss > totalProfit:
+				totalLossBiggerThanTotalProfit += 1
+
+			# drawPlot(tick, (tick - orderPlaceTick), openAudUsd[(orderPlaceTick-window) : tick], False)
+
 		if tickDiff > 0 and abs(tickDiff) >= stopProfit:
 			isOrderPlaced = False
 			totalProfit += abs(tickDiff)
+			# drawPlot(tick, (tick - orderPlaceTick), openAudUsd[(orderPlaceTick-window) : tick], True)
 
 print('Total profit:', totalProfit)
 print('Total loss:', totalLoss)
 print('Profit:', totalProfit - totalLoss)
+print('Bellow zero:', totalLossBiggerThanTotalProfit)
